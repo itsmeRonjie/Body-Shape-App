9491bf5d0cfc88eb79c0795df239fd65ef8fdc9d
Ronjie Diafante Man-on <rdmanon@vauldex.com>
2025-05-20 15:45:52 +0800

    - fixed error in ScreenConstants - added IconRightView

--- Body-Shape-App/Constants/Screen.swift
+++ Body-Shape-App/Constants/Screen.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import SwiftUI
 
 let screenWidth = UIScreen.main.bounds.width
 let screenHeight = UIScreen.main.bounds.height
---
+++ Body-Shape-App/View/Sub View/IconRight.swift
@@ -0,0 +1,34 @@
+//
+//  IconRight.swift
+//  Body-Shape-App
+//
+//  Created by Ronjie Diafante Man-on on 5/20/25.
+//
+
+import SwiftUI
+
+struct IconRight: View {
+    let imageName: String
+    var angle = 0.0
+    var body: some View {
+        HStack {
+            Spacer()
+            Image(systemName: imageName)
+                .foregroundColor(.white)
+                .padding()
+                .background(Color.black)
+                .clipShape(Circle())
+                .rotation3DEffect(.degrees(angle), axis: (x: 0, y: 0, z: 1))
+        }
+    }
+}
+
+struct IconRight_Previews: PreviewProvider {
+    static var previews: some View {
+        VStack {
+            IconRight(imageName: "circle.grid.cross.fill")
+                .padding()
+            Spacer()
+        }
+    }
+}
