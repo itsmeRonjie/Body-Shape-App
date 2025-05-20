faa6c8b0d51bfaf90a5901e7b6072975107e28ec
Ronjie Diafante Man-on <rdmanon@vauldex.com>
2025-05-20 15:43:39 +0800

    - added resources and constants

---
+++ Body-Shape-App/Constants/Colors.swift
@@ -0,0 +1,19 @@
+//
+//  Colors.swift
+//  Body-Shape-App
+//
+//  Created by Ronjie Diafante Man-on on 5/20/25.
+//
+
+import Foundation
+import SwiftUI
+
+let lightGreen = Color("lightGreen")
+let lightPurple = Color("lightPurple")
+
+let person1 = "pexels-heitor-verdi-2169434"
+let person2 = "pexels-ingrid-santana-2100063"
+let person3 = "pexels-italo-melo-2379005"
+let person4 = "pexels-simon-robben-614810"
+
+let bikeImage = "Bike Image"
---
+++ Body-Shape-App/Constants/Screen.swift
@@ -0,0 +1,11 @@
+//
+//  Screen.swift
+//  Body-Shape-App
+//
+//  Created by Ronjie Diafante Man-on on 5/20/25.
+//
+
+import Foundation
+
+let screenWidth = UIScreen.main.bounds.width
+let screenHeight = UIScreen.main.bounds.height
