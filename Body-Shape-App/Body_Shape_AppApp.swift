c0e50bb6c57c4de1093f7318f4716ba601e22b23
Ronjie Diafante Man-on <rdmanon@vauldex.com>
2025-05-20 15:39:21 +0800

    - initialized project

---
+++ Body-Shape-App/Body_Shape_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Body_Shape_AppApp.swift
+//  Body-Shape-App
+//
+//  Created by Ronjie Diafante Man-on on 5/20/25.
+//
+
+import SwiftUI
+
+@main
+struct Body_Shape_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            BodyShapeMainView()
+        }
+    }
+}
---
+++ Body-Shape-App/View/Main View/BodyShapeMainView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Body-Shape-App
+//
+//  Created by Ronjie Diafante Man-on on 5/20/25.
+//
+
+import SwiftUI
+
+struct BodyShapeMainView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    BodyShapeMainView()
+}
